094427b6276db1d92dfaf8a034e23cdd1bcc86ff
granx18 <729354837@qq.com>
2019-04-14 12:12:33 +0800

    0414通知栏接口修改

--- app/src/main/java/com/example/moreex/view/login/IActivityOne.java
+++ app/src/main/java/com/example/moreex/view/login/IActivityOne.java
@@ -2,6 +2,10 @@ package com.example.moreex.view.login;
 
 import com.example.moreex.view.BaseView;
 
+import java.util.List;
+
+import io.swagger.client.model.Notice;
+
 public interface IActivityOne extends BaseView {
     void onSuccess();
 
@@ -9,5 +13,5 @@ public interface IActivityOne extends BaseView {
 
     void showWrongToast();
 
-    void onSuccessNotice(String title,String detail);
+    void onSuccessNotice(List<Notice> list);
 }
